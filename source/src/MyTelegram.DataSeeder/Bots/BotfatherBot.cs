9c1f56cfe873a6c34d117847bc8dafabde60d3b6
Tu Nombre <you@example.com>
2025-08-11 16:44:09 +0200

    egg1 commit

--- source/src/MyTelegram.DataSeeder/Bots/BotfatherBot.cs
+++ source/src/MyTelegram.DataSeeder/Bots/BotfatherBot.cs
@@ -5,21 +5,27 @@ namespace MyTelegram.DataSeeder.Bots;
 
 public class BotfatherBot
 {
-    private readonly ITelegramBotClient _botClient;
+    private readonly ITelegramBotClient? _botClient;
     private readonly string? _webhookUrl;
 
     public BotfatherBot(IOptions<MyTelegramDataSeederOptions> options)
     {
         var botOptions = options.Value.MyTelegramBotOptions;
+        _webhookUrl = botOptions.BotFatherWebHookUrl;
+
+        if (string.IsNullOrWhiteSpace(botOptions.BotFatherToken))
+        {
+            return;
+        }
+
         var baseUrl = botOptions.BotApiBaseUrl ?? "https://api.buzzster.io";
         var botClientOptions = new TelegramBotClientOptions(botOptions.BotFatherToken, baseUrl);
         _botClient = new TelegramBotClient(botClientOptions);
-        _webhookUrl = botOptions.BotFatherWebHookUrl;
     }
 
     public async Task EnsureWebhookAsync()
     {
-        if (!string.IsNullOrEmpty(_webhookUrl))
+        if (_botClient != null && !string.IsNullOrEmpty(_webhookUrl))
         {
             await _botClient.SetWebhook(_webhookUrl);
         }
